64dddbe45c6615fbfce0252ab8e7dc99675c9c44
AnkurVyas-BTC <ankur.vyas@beaverlogic.com>
2014-07-08 15:40:48 +0530

    deadline date sorting

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :client
-  default_scope -> { order('created_at DESC')}
+  default_scope -> { order('deadline_date DESC')}
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :description, presence: true
 	validates :billing_type, presence: true
